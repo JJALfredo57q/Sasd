9d51210ad7ec41f8c2fbf74013df00ff498f513a
JJALfredo57q <115348840+JJALfredo57q@users.noreply.github.com>
2024-05-12 16:39:19 +0800

    Add files via upload

---
+++ Home/asd.js
@@ -0,0 +1,7 @@
+function can(value) {
+   var sd =  document.getElementById(value).value
+
+   if (sd == 1 ) {
+    alert('Canceled')
+   }
+}
---
+++ java.js
@@ -0,0 +1,9 @@
+function button1 () {
+    var num = document.getElementById('button1').value 
+
+    if (num === 1) {
+        document.getElementById('Display').value = 'Loading...'
+
+    }
+
+}
